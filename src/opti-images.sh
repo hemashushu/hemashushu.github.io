71c29e19d895a1db76d850dcc121d9141adb00c9
Hippo Spark (Arch) <hippospark@gmail.com>
2024-02-13 01:14:19 +0800

    add webp support.

---
+++ src/opti-images.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# there are many ways to optimize images, such as:
+# - jpegoptim
+# - optipng
+# - pngcrush
+# however, a more efficient way to optimize PNG is
+# convert it to WEBP format.
+
+FILES=$(find ./content -type f -name "*.png")
+for NAMEPATH in $FILES; do
+	#echo $NAMEPATH
+	NAMEPATH_WITHOUT_EXTENSION=${NAMEPATH%.*}
+	FROM="$NAMEPATH_WITHOUT_EXTENSION.png" 
+        TO="$NAMEPATH_WITHOUT_EXTENSION.webp"
+	cwebp "$FROM" -o "$TO"
+done
+
